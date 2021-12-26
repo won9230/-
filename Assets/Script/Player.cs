7dd091a095d7f51b260c216512879a07b4be29cd
won9230 <chldnjs8899@gmail.com>
2021-12-27 02:00:08 +0900

    no message

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Player : MonoBehaviourPunCallbacks
+{
+	private float h, v;
+	private Rigidbody rigidbody;
+
+	public float speed = 5f;
+
+	private void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		if (!photonView.IsMine)
+		{
+			return;
+		}
+
+		v = Input.GetAxisRaw("Vertical");
+		h = Input.GetAxisRaw("Horizontal");
+
+		transform.Translate(Vector3.forward * v * speed * Time.deltaTime);
+		transform.Translate(Vector3.right * h * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Script/muit/GameMgr.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMgr : MonoBehaviourPunCallbacks
+{
+	
+}
